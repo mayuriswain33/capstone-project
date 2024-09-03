81fac1d9e67c502c95565bb85161531915209be6
mayuriswain33 <mayuriswain33@gmail.com>
2024-09-03 23:18:30 +0530

    capstone project review comments

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -1,31 +1,38 @@
 export default async function decorate(block) {
   const articleLists = block.querySelector('a[href$=".json"]');
+
   async function loadArticles() {
     const resp = await fetch(articleLists.href);
     const json = await resp.json();
 
-    json.data.forEach((row) => {
-      // Create the elements
+    const data = json.data.filter((row) => row.template === 'magazine');
+
+    data.forEach((row) => {
       const articleDiv = document.createElement('div');
       articleDiv.classList.add('article-item');
+
       const link = document.createElement('a');
-      link.href = row.url;
+      link.href = row.path;
+
       const img = document.createElement('img');
       img.src = row.image;
       img.alt = row.name;
+
       const title = document.createElement('h3');
-      title.textContent = row.name;
+      title.textContent = row.title;
+
       const paragraph = document.createElement('p');
       paragraph.textContent = row.description;
 
-      // Append the elements to their parents
       link.appendChild(img);
       link.appendChild(title);
       articleDiv.appendChild(link);
       articleDiv.appendChild(paragraph);
+
       block.appendChild(articleDiv);
-      articleLists.replaceWith();
     });
+
+    articleLists.replaceWith();
   }
 
   if (articleLists) {
