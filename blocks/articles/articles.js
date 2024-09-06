b6677fff64c5d3585a941c01581228a5c60b587a
mayuriswain33 <mayuriswain33@gmail.com>
2024-09-06 20:26:41 +0530

    Responsive design

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -1,41 +1,49 @@
 export default async function decorate(block) {
-  const articleLists = block.querySelector('a[href$=".json"]');
+  const createArticleElement = (article) => {
+    const articleDiv = document.createElement('div');
+    articleDiv.classList.add('article-item');
 
-  async function loadArticles() {
-    const resp = await fetch(articleLists.href);
-    const json = await resp.json();
+    const link = document.createElement('a');
+    link.href = article.path;
 
-    const data = json.data.filter((row) => row.template === 'magazine');
+    const img = document.createElement('img');
+    img.src = article.image;
+    img.alt = article.name;
 
-    data.forEach((row) => {
-      const articleDiv = document.createElement('div');
-      articleDiv.classList.add('article-item');
+    const title = document.createElement('h3');
+    title.textContent = article.title;
 
-      const link = document.createElement('a');
-      link.href = row.path;
+    const paragraph = document.createElement('p');
+    paragraph.textContent = article.description;
 
-      const img = document.createElement('img');
-      img.src = row.image;
-      img.alt = row.name;
+    link.appendChild(img);
+    link.appendChild(title);
+    articleDiv.appendChild(link);
+    articleDiv.appendChild(paragraph);
 
-      const title = document.createElement('h3');
-      title.textContent = row.title;
+    return articleDiv;
+  };
 
-      const paragraph = document.createElement('p');
-      paragraph.textContent = row.description;
+  const loadArticles = async (data) => {
+    try {
+      const response = await fetch(data);
+      if (!response.ok) throw new Error(`Error fetching data: ${response.statusText}`);
+      const json = await response.json();
+      const filteredArticles = json.data.filter((article) => article.template === 'magazine');
+      filteredArticles.forEach((article) => {
+        const articleElement = createArticleElement(article);
+        block.appendChild(articleElement);
+      });
+    } catch (error) {
+      // console.log(error);
+    }
+  };
 
-      link.appendChild(img);
-      link.appendChild(title);
-      articleDiv.appendChild(link);
-      articleDiv.appendChild(paragraph);
+  const articleListLink = block.querySelector('a[href$=".json"]');
 
-      block.appendChild(articleDiv);
-    });
-
-    articleLists.replaceWith();
-  }
-
-  if (articleLists) {
-    loadArticles();
+  if (articleListLink) {
+    const articledata = articleListLink.href;
+    await loadArticles(articledata);
+    articleListLink.remove();
   }
 }
